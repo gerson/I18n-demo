772e14675e581b9339b9a1974e063f9cbfef6f08
Gerson Villanueva <faillaceg@gmail.com>
2016-08-02 02:08:57 -0300

    Internationalization and localization of apps - example

--- I18n Demo/ViewController.swift
+++ I18n Demo/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textLabel: UILabel!
+    
+    @IBOutlet var currencyLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        populateValues()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var currencyFormatter: NSNumberFormatter {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        return formatter
     }
-
-
+    
+    var dateFormatter: NSDateFormatter {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .MediumStyle
+        return formatter
+    }
+    
+    
+    private func populateValues() {
+        textLabel.text = NSLocalizedString("HELLO_WORLD", comment: "Hello world")
+        currencyLabel.text = currencyFormatter.stringFromNumber(10000)
+        dateLabel.text = dateFormatter.stringFromDate(NSDate())
+        imageView.image = UIImage(named: "flag")
+    }
+    
 }
-
